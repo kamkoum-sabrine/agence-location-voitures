93f19971b7c907a58d2180f70e0eb91cf9a6a5e9
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2024-10-16 20:23:31 +0100

    change package name

--- src/main/java/agencelocation/vehicule/VoitureCommerciale.java
+++ src/main/java/agencelocation/vehicule/VoitureCommerciale.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package location.vehicule.agencelocation;
+package agencelocation.vehicule;
 
 import java.util.Date;
 
