1a6e72038be778d3925af53f765ac97d6a2f05a4
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2024-10-16 20:16:18 +0100

    firt commit

---
+++ src/main/java/location/vehicule/agencelocation/AgenceLocation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package location.vehicule.agencelocation;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class AgenceLocation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
